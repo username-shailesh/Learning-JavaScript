b08c9c856be8892a7613eacfc34c93323dc5da53
Shailesh Maurya <161061492+shailesh-maurya-official@users.noreply.github.com>
2024-02-24 10:24:34 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";  // treat all JS Code as newer version
+
+// alert(3 + 5) // we are using nodejs, not browser
+
+console.log(5 + 
+    5)          //  code readability should be high
+
+console.log("Shailesh")
+
+let name = "shailesh"
+let age = 18
+let isLoggedIn = false
+
+// number --> 2 to power 53
+// bigint
+// string --> ""
+// boolean --> true/false
+// null --> standalone datatype
+// symbol --> unique
+// undefined
+//object
+
+console.log(typeof 6)  //number
+console.log(typeof null)  //object
