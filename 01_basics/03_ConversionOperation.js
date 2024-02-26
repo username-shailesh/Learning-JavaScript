c0ff49bfac0e834ce74ce8ffef50596e1961c27f
Shailesh Maurya <161061492+shailesh-maurya-official@users.noreply.github.com>
2024-02-26 10:02:23 +0000

    Conversion Operation

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,28 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let ValueInNumber = Number(score)
+// console.log(typeof ValueInNumber);
+// console.log(ValueInNumber);
+
+
+// 33 --> 33
+// 32abc --> NaN
+// true --> 1     // false --> 0
+
+let isLoggedIn = "Shailesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 --> true  ;  0 --> false
+// "" --> false
+// "Shailesh" --> true
+
+let someNumber = 34
+
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
