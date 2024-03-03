62fa3d54629eefa99d47ddb320ee5f1187a1e3af
Shailesh Maurya <161061492+shailesh-maurya-official@users.noreply.github.com>
2024-03-03 02:56:48 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -39,3 +39,40 @@ function loginUserMessage(username = "Sha"){
 
 // console.log(loginUserMessage("Shailesh"))
 console.log(loginUserMessage("Shailesh"))
+
+
+//   ****************************************************************************
+
+function addCartPrice(value1, value2, ...num1){
+    return num1
+        
+}
+
+// console.log(addCartPrice(100, 200, 500, 2000))
+
+const user = {
+    username: "Shailesh",
+    price: 399
+}
+
+function handleObject(anyObject){
+    console.log((`Username is ${anyObject.username} and price is ${anyObject.price}`));
+}
+
+// handleObject(user)
+
+handleObject({
+    username: "Sha",
+    price: 499
+})
+    
+
+const myNewArray = [200, 300, 400, 500]
+
+function mySecondValue(getArray){
+    return getArray[1]
+
+}
+
+// console.log(mySecondValue(myNewArray));
+console.log(mySecondValue([200, 300, 400, 500]));
